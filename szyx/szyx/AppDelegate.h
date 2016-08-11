fb23ad97eca074e574c3b5a7a88c9d0ca5cbb946
邓超文 <bbdengchaowen@qq.com>
2016-08-11 12:45:45 +0800

    创建项目

---
+++ szyx/szyx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  szyx
+//
+//  Created by 邓超文 on 16/8/11.
+//  Copyright © 2016年 szyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ szyx/szyx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  szyx
+//
+//  Created by 邓超文 on 16/8/11.
+//  Copyright © 2016年 szyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
